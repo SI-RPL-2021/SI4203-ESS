76509ccbf1f610bc4b94747e2251bd8074286e19
GASOT\Gasot <naufalhanifr001@gmail.com>
2021-05-02 22:33:24 +0700

    Menginstall spatie laravel permission

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {
@@ -65,11 +66,14 @@ class RegisterController extends Controller
 
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-            'level' => 'user'
+            'username' => Str::snake($data['name']),
+            'password' => Hash::make($data['password'])
         ]);
+
+        $user->assignRole('user');
+
+        return $user;
     }
 }
