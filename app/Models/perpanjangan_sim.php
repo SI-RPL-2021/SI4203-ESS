f8fd4852632fb383cf279223091902728bd90f2d
Annisa Fitriani <71880105+Annisa190516@users.noreply.github.com>
2021-05-06 21:45:13 +0700

    Update perpanjangan SIM

--- app/Models/perpanjangan_sim.php
+++ app/Models/perpanjangan_sim.php
@@ -10,17 +10,16 @@ class perpanjangan_sim extends Model
     use HasFactory;
     protected $table = "perpanjangan_sim";
     protected $guarded = [];
-<<<<<<< HEAD
+
     
     public function user()
     {
         return $this->hasOne(User::class);
-=======
 
     public $dates = ['masa_berlaku'];
     public function sim()
     {
         return $this->belongsTo('App\Models\pembuatan_sim', 'sim_id', 'id');
->>>>>>> 8b71f03d3b1efbdbc5235b93527cd718b8a0feca
+
     }
 }
