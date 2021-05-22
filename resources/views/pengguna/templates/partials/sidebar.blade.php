e73ade5ef7a279ddabb4932bdf7386335b3c2650
Annisa Fitriani <71880105+Annisa190516@users.noreply.github.com>
2021-05-23 00:57:39 +0700

    Update sidebar.blade.php

--- resources/views/pengguna/templates/partials/sidebar.blade.php
+++ resources/views/pengguna/templates/partials/sidebar.blade.php
@@ -28,7 +28,7 @@
 
         <!-- Nav Item - Dashboard -->
         <li class="nav-item">
-            <a class="nav-link" href="{{ url('home') }}">
+            <a class="nav-link" href="{{ url('dashboard') }}">
                 <i class="fas fa-fw fa-home"></i>
                 <span>Dashboard</span></a>
         </li>
@@ -38,7 +38,7 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item">
-        <a class="nav-link" href="{{ route('dashboard') }}">
+        <a class="nav-link" href="{{ route('ddashboard') }}">
             <i class="fas fa-fw fa-home"></i>
             <span>Dashboard</span></a>
     </li>
